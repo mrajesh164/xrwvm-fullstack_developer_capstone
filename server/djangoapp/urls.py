1489434aff5236e290e1e06ab83427f46c82eb2f
mrajesh164 <mrajesh@uchicago.edu>
2024-09-23 13:38:55 -0400

    updating changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for get dealerships view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
         path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for get dealer details
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,17 @@
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import PostReview from "./components/Dealers/PostReview"
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
